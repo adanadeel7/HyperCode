1ab8d649931f24b7c87508c46d9bf9a9cb70f7f3
Adan Adeel <adanadeel903@gmail.com>
2026-08-19 13:01:47 +0500

    Updated

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,7 @@ const app = express()
 
 // 1. Configure Global Middlewares First
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", 
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true, 
 }));
